ee16c7605b786c360ca9dfe0bdd337e9b1901144
tentone <tentone@outlook.com>
2022-03-16 19:06:16 +0000

    Derp

--- stenographer/stenographer.cpp
+++ stenographer/stenographer.cpp
@@ -1,23 +1,6 @@
-﻿#include <opencv2\opencv.hpp>
-
-using namespace cv;
-using namespace std;
+﻿using namespace std;
 
 int main() {
-	Mat image;
-
-	namedWindow("Display window");
-	VideoCapture cap(0);
-
-	if (!cap.isOpened()) {
-		cout << "cannot open camera";
-	}
-
-	while (true) {
-		cap >> image;
-		imshow("Display window", image);
-		waitKey(25);
-	}
 
 	return 0;
 
