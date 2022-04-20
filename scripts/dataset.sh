ef058d689e1d541e29de58ae112c1efc8b5831ae
tentone <tentone@outlook.com>
2022-04-20 15:13:49 +0100

    Placeholder

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-echo " - Update Aptitude"
-apt update
+if (( $EUID > 0 )); then
+	echo " - Please run as root to install dependencies"
+else
+    echo " - Update Aptitude"
+    apt update
 
-echo " - Install Python 3"
-apt install python3 python3-pip
+    echo " - Install Python 3"
+    apt install python3 python3-pip
 
-echo " - Install youtube-dl"
-pip3 install --upgrade yt-dlp
+    echo " - Install youtube-dl"
+    pip3 install --upgrade yt-dlp
+fi
+
 echo " - Download dataset"
-
 echo "    - Highway traffic"
-yt-dlp https://www.youtube.com/watch?v=nt3D26lrkho -o highway-traffic.mp4
+yt-dlp https://www.youtube.com/watch?v=nt3D26lrkho -o ../dataset/highway-traffic.mp4
 
 echo "    - Highway Traffic Low Camera Shake"
-yt-dlp https://www.youtube.com/watch?v=KBsqQez-O4w -o highway-traffic-shake.mp4
+yt-dlp https://www.youtube.com/watch?v=KBsqQez-O4w -o ../dataset/highway-traffic-shake.mp4
 
 echo "    - Night City Traffic"
-yt-dlp https://www.youtube.com/watch?v=iA0Tgng9v9U -o city-traffic-night.mp4
+yt-dlp https://www.youtube.com/watch?v=iA0Tgng9v9U -o ../dataset/city-traffic-night.mp4
 
 echo " - Done!"
