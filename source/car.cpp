cdfc2319aed7ad98d5cb774683d8a2ea30d185e5
tentone <tentone@outlook.com>
2022-04-23 23:23:15 +0100

    Car

---
+++ source/car.cpp
@@ -0,0 +1,22 @@
+// Possible car colors detected
+enum Color { red, green, blue, black, white, grey };
+
+// Description of the car
+class Car {
+    private: 
+        static int _id;
+
+    public:
+        // Identifier (sequential)
+        int id;
+
+        // License plate 
+
+        // Color of the car
+        Color color;
+        
+        Car() {
+            this->id = Car::_id++;
+            this->color = black;
+        }
+};
