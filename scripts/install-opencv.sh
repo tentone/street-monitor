5122064498a65a54793e2e0c48644dff89394ed8
tentone <tentone@outlook.com>
2022-05-21 21:55:39 +0100

    Derp

--- scripts/install-opencv.sh
+++ scripts/install-opencv.sh
@@ -7,6 +7,7 @@ fi
 
 # Version of opencv to install
 VERSION='4.5.5'
+CUDA=true
 
 echo "- Create libs folder"
 mkdir -p ../libs
@@ -19,7 +20,7 @@ echo " - Install Build tools"
 apt install -y cmake gcc g++ make git unzip curl
 
 echo " - Installing Dependencies"
-apt install -y libavcodec-dev libavformat-dev libswscale-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev
+apt install -y libavcodec-dev libavformat-dev libswscale-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev libtbb-dev libatlas-base-dev gfortran
 
 echo " - Install OpenCV Contrib Modules"
 echo "    - Clone repository"
@@ -44,7 +45,7 @@ git checkout $VERSION
 echo "    - Generate makefile"
 mkdir build
 cd build
-cmake -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules/ -D OPENCV_ENABLE_NONFREE=1 .. -D WITH_CUDA=ON -D WITH_CUDNN=OFF -D OPENCV_DNN_CUDA=ON -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1
+cmake -D ENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules/ -D BUILD_EXAMPLES=OFF -D WITH_V4L=ON -D WITH_TBB=ON -D OPENCV_ENABLE_NONFREE=1 -D ENABLE_FAST_MATH=1  .. #-D WITH_CUDA=ON -D WITH_CUDNN=ON -D OPENCV_DNN_CUDA=ON -D CUDA_FAST_MATH=1 -D CUDA_ARCH_BIN=7.5 .. 
 
 echo "    - Build and Install"
 make install -j8
